29001f31e9cd17c2e4c41f355acbc0f9905987bf
Robbe Van Herck <robbevanherck1@gmail.com>
2018-01-09 22:45:54 +0100

    Fixed disabling of buttons

--- index.js
+++ index.js
@@ -19,29 +19,34 @@ function callback(field, out, isError = false) {
   if(!isError) {
     setButtonState(field, "SEARCH")
     document.getElementById("StopsList"+field).innerHTML = ""
+    if(out.haltes.length == 0) {
+      document.getElementById("StopsList"+field).innerHTML = "No stops found"
+    }
     for (var i = 0; i < Math.min(out.haltes.length,5); i++) {
       document.getElementById("StopsList"+field).innerHTML += `<li>${out.haltes[i].omschrijvingLang}</li>`
     }
   } else {
     setButtonState(field, "ERROR")
-    document.getElementById("StopsList"+field).innerHTML = `<li class="Error">An error occured</li>`
+    document.getElementById("StopsList"+field).innerHTML = `<div class="Error">An error occured</div>`
   }
 }
 
 function setButtonState(field, state) {
   switch (state) {
     case "LOADING":
-    document.getElementById("BtnValue"+field).innerHTML = "&#x21bb;"
-    document.getElementById("BtnValue"+field).className += " Loading"
-    document.getElementById("BtnValue"+field).disabled = true;
+      document.getElementById("BtnValue"+field).innerHTML = "&#x21bb;"
+      document.getElementById("BtnValue"+field).className += " Loading"
+      document.getElementById("Btn"+field).disabled = true;
     break;
     case "SEARCH":
-    document.getElementById("BtnValue"+field).innerHTML = "&#128269;"
-    document.getElementById("BtnValue"+field).className = document.getElementById("BtnValue"+field).className.replace(" Loading", "")
+      document.getElementById("BtnValue"+field).innerHTML = "&#128269;"
+      document.getElementById("BtnValue"+field).className = document.getElementById("BtnValue"+field).className.replace(" Loading", "")
+      document.getElementById("Btn"+field).disabled = false;
     break;
     case "ERROR":
-    document.getElementById("BtnValue"+field).innerHTML = "&#x2716;"
-    document.getElementById("BtnValue"+field).className = document.getElementById("BtnValue"+field).className.replace(" Loading", "")
+      document.getElementById("BtnValue"+field).innerHTML = "&#x2716;"
+      document.getElementById("BtnValue"+field).className = document.getElementById("BtnValue"+field).className.replace(" Loading", "")
+      document.getElementById("Btn"+field).disabled = false;
     break;
     default:
 
